8764542f157d6d39617d559c657db2d497679bc6
AzUsA <33446494+azusa0999@users.noreply.github.com>
2018-02-28 19:35:52 +0900

    초기 업데이트
    초기 업데이트

---
+++ sci-hubAutoLink/ScihubVal.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace sci_hubAutoLink
+{
+    partial class ScihubVal
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxLinkItem = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxCount = new System.Windows.Forms.TextBox();
+            this.lblGuide = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxLinkItem
+            // 
+            this.cbxLinkItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxLinkItem.FormattingEnabled = true;
+            this.cbxLinkItem.Location = new System.Drawing.Point(50, 3);
+            this.cbxLinkItem.Name = "cbxLinkItem";
+            this.cbxLinkItem.Size = new System.Drawing.Size(252, 23);
+            this.cbxLinkItem.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(9, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(108, 25);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.57143F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.42857F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(460, 105);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.58442F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.41558F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 148F));
+            this.tableLayoutPanel2.Controls.Add(this.btnGo, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbxLinkItem, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 54);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(454, 48);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGo.Location = new System.Drawing.Point(308, 3);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(143, 42);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblGuide);
+            this.panel1.Controls.Add(this.tbxCount);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(454, 45);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "검색 남은 횟수 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "회";
+            // 
+            // tbxCount
+            // 
+            this.tbxCount.Location = new System.Drawing.Point(369, 11);
+            this.tbxCount.Name = "tbxCount";
+            this.tbxCount.ReadOnly = true;
+            this.tbxCount.Size = new System.Drawing.Size(41, 25);
+            this.tbxCount.TabIndex = 6;
+            this.tbxCount.Text = "0";
+            this.tbxCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblGuide
+            // 
+            this.lblGuide.AutoSize = true;
+            this.lblGuide.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblGuide.Location = new System.Drawing.Point(131, 12);
+            this.lblGuide.Name = "lblGuide";
+            this.lblGuide.Size = new System.Drawing.Size(72, 15);
+            this.lblGuide.TabIndex = 7;
+            this.lblGuide.Text = "확인중....";
+            // 
+            // ScihubVal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 105);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ScihubVal";
+            this.Text = "sci-hub Link Validation";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxLinkItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxCount;
+        private System.Windows.Forms.Label lblGuide;
+    }
+}
+
---
+++ sci-hubAutoLink/ScihubVal.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace sci_hubAutoLink
+{
+    public partial class ScihubVal : Form
+    {
+        public ScihubVal()
+        {
+            InitializeComponent();
+            lblGuide.Visible = false;
+        }
+
+        static int totalCount = 0;
+        Object thisLock = new Object();
+        private void Check()
+        {
+            string alphabat = "abcdefghijklmnopqrstuvwxyz";
+            //string alphabat = "ahzk";
+            char[] indx1 = alphabat.ToCharArray();
+            char[] indx2 = alphabat.ToCharArray();
+            string scihublink = "http://www.sci-hub.";
+            //모든 경우의 수는 26^2 = 676개의 루프가 필요.
+            totalCount = (int)Math.Pow(indx1.Length, 2);
+            this.SetText(totalCount.ToString());
+            this.lblGuide.Visible = true;
+            Application.DoEvents();
+
+            foreach (char chr1 in indx1)
+            {
+                foreach (char chr2 in indx2)
+                {
+                    string who = scihublink + chr1 + chr2;
+
+                    //Thread trd = new Thread(new ParameterizedThreadStart(isConnSuccess));
+                    Thread trd = new Thread(delegate ()
+                    {
+                        isConnSuccess(who);
+                    }
+                    );
+
+                    trd.Start();
+                }
+            }
+        }
+
+        private void isConnSuccess(string url)
+        {
+            try
+            {
+                Ping pingSender = new Ping();
+                Uri uri = new Uri(url);
+                var ip = Dns.GetHostAddresses(uri.Host)[0];
+                PingReply reply = pingSender.Send(ip);
+                if (reply.Status == IPStatus.Success)
+                {
+                    //this.cbxLinkItem.Items.Add(url);
+                    this.SetAddItem(url);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            lock (thisLock)
+            {
+                totalCount--;
+                this.SetText(totalCount.ToString());
+                SetVisible(totalCount > 0);
+                Application.DoEvents();
+            }
+        }
+        
+        private void SetAddItem(string text)
+        {
+            if (this.cbxLinkItem.InvokeRequired)
+            {
+                this.cbxLinkItem.BeginInvoke(new Action(() => cbxLinkItem.Items.Add(text)));
+            }
+            else
+            {
+                this.cbxLinkItem.Items.Add(text);
+            }
+        }
+
+        private void SetText(string text)
+        {
+            if (this.tbxCount.InvokeRequired)
+            {
+                this.tbxCount.BeginInvoke(new Action(() => tbxCount.Text = text));
+            }
+            else
+            {
+                this.tbxCount.Text = text;
+            }
+        }
+        
+        private void SetVisible(bool isVisible)
+        {
+            if (this.lblGuide.InvokeRequired)
+            {
+                this.lblGuide.BeginInvoke(new Action(() => lblGuide.Visible = isVisible));
+            }
+            else
+            {
+                this.lblGuide.Visible = isVisible;
+            }
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.Check();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(cbxLinkItem.Text);
+        }
+
+    }
+}
